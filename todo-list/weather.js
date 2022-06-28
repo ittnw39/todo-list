a8a2078f3b76e49985fddddd0206d4752cac9f77
ittnw39 <ittnw39@gmail.com>
2022-06-28 22:37:42 +0900

    todoweather

--- todo-list/weather.js
+++ todo-list/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "7384c1771e5ca144490973d82d5604ea";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}$units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(response => response.json())
     .then(data =>{
